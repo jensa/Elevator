112d9af94a1a11aa73b9a7135c2e33d3e9a02f3a
Jens Arvidsson <jensarv@gmail.com>
2013-02-27 13:25:44 +0100

    sound.wav

--- src/controller/ElevatorThread.java
+++ src/controller/ElevatorThread.java
@@ -1,21 +1,40 @@
 package controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.rmi.RemoteException;
 import java.util.concurrent.ConcurrentLinkedDeque;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import Orders.Order;
 
 import elevator.rmi.Elevator;
 import elevator.rmi.Motor;
+import java.applet.*;
 
 public class ElevatorThread implements Runnable{
 	RMI controller;
 	int id;
+	private AudioClip bing;
 	private boolean isMoving = false;
 	private ConcurrentLinkedDeque<Order> elevatorOrders = new ConcurrentLinkedDeque<Order> ();
 	
 	public ElevatorThread (int id, RMI controller){
 		this.controller = controller;
 		this.id = id;
+		URL soundPath = null;
+		try {
+			soundPath = new URL("sound.wav");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} // Get the Sound URL
+		bing = Applet.newAudioClip(soundPath);
 	}
 
 	@Override
@@ -44,6 +63,7 @@ public class ElevatorThread implements Runnable{
 	
 	private void openDoor () throws RemoteException{
 		controller.getDoor (id).open ();
+		bing.play ();
 		try {
 			Thread.sleep (2000);
 		} catch (InterruptedException e1) {
